a2e8ae6f367b8f7026c74f70fe3bc88fd7431b1a
Tyler Fisher <tfisher@politico.com>
2018-02-15 15:08:55 -0500

    add search field to admin

--- government/admin.py
+++ government/admin.py
@@ -6,6 +6,7 @@ from .models import Body, Jurisdiction, Office, Party
 class OfficeAdmin(admin.ModelAdmin):
     list_display = ('name',)
     list_filter = ('body__label', 'jurisdiction__name',)
+    search_fields = ['name']
 
 
 admin.site.register(Body)
