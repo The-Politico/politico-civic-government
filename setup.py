a0cb6e00ba3532f70a7aa0ea64e0a97b54cdd8e3
Tyler Fisher <tfisher@politico.com>
2018-11-15 11:04:42 -0500

    catchall bootstrap command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="politico-civic-government",
-    version="0.1.3",
+    version="0.1.4",
     description="",
     url="https://github.com/The-Politico/politico-civic-government",
     author="POLITICO interactive news",
