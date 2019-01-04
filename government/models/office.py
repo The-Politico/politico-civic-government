fd28d0683e9d7cc20a7bc0ec24342740f03a59d1
Tyler Fisher <tfisher@politico.com>
2019-01-04 14:33:08 -0500

    change str representation of office

--- government/models/office.py
+++ government/models/office.py
@@ -24,21 +24,19 @@ class Office(models.Model):
         - florida/house/seat-2/
         - michigan/house/seat-2/
     """
-    FIRST_CLASS = '1'
-    SECOND_CLASS = '2'
-    THIRD_CLASS = '3'
+
+    FIRST_CLASS = "1"
+    SECOND_CLASS = "2"
+    THIRD_CLASS = "3"
 
     SENATE_CLASSES = (
-        (FIRST_CLASS, '1st Class'),
-        (SECOND_CLASS, '2nd Class'),
-        (THIRD_CLASS, '3rd Class'),
+        (FIRST_CLASS, "1st Class"),
+        (SECOND_CLASS, "2nd Class"),
+        (THIRD_CLASS, "3rd Class"),
     )
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    uid = models.CharField(
-        max_length=500,
-        editable=False,
-        blank=True)
+    uid = models.CharField(max_length=500, editable=False, blank=True)
 
     slug = models.SlugField(blank=True, max_length=255, editable=True)
     name = models.CharField(max_length=255)
@@ -49,16 +47,25 @@ class Office(models.Model):
     )
 
     division = models.ForeignKey(
-        Division, related_name='offices', on_delete=models.PROTECT)
+        Division, related_name="offices", on_delete=models.PROTECT
+    )
     jurisdiction = models.ForeignKey(
-        'Jurisdiction', null=True, blank=True, related_name='offices',
-        on_delete=models.PROTECT)
+        "Jurisdiction",
+        null=True,
+        blank=True,
+        related_name="offices",
+        on_delete=models.PROTECT,
+    )
     body = models.ForeignKey(
-        'Body', null=True, blank=True, related_name='offices',
-        on_delete=models.PROTECT)
+        "Body",
+        null=True,
+        blank=True,
+        related_name="offices",
+        on_delete=models.PROTECT,
+    )
 
     def __str__(self):
-        return self.uid
+        return self.label
 
     @property
     def is_executive(self):
@@ -69,9 +76,8 @@ class Office(models.Model):
         """
         **uid**: :code:`{body.uid | jurisdiction.uid}_office:{slug}`
         """
-        stripped_name = ' '.join(
-            w for w in self.name.split()
-            if w not in STOPWORDS
+        stripped_name = " ".join(
+            w for w in self.name.split() if w not in STOPWORDS
         )
 
         if not self.slug:
@@ -79,14 +85,16 @@ class Office(models.Model):
                 stripped_name,
                 instance=self,
                 max_length=100,
-                separator='-',
-                start_no=2
+                separator="-",
+                start_no=2,
             )
         if self.body:
-            self.uid = '{}_office:{}'.format(
-                self.body.uid, slugify(stripped_name))
+            self.uid = "{}_office:{}".format(
+                self.body.uid, slugify(stripped_name)
+            )
         else:
-            self.uid = '{}_office:{}'.format(
-                self.jurisdiction.uid, slugify(stripped_name))
+            self.uid = "{}_office:{}".format(
+                self.jurisdiction.uid, slugify(stripped_name)
+            )
 
         super(Office, self).save(*args, **kwargs)
