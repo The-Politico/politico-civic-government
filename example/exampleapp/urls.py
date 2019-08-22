f37310318758e1a6ab49f2abf1ea67191f32b272
Allan James Vestal <allanjamesvestal@gmail.com>
2019-08-22 16:56:58 -0400

    Blacken example app and standardize its imports.

--- example/exampleapp/urls.py
+++ example/exampleapp/urls.py
@@ -1,7 +1,9 @@
+# Imports from Django.
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('government.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("government.urls")),
 ]
