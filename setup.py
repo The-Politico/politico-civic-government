64aa017bc6e6a4d3a235610838a0252e8c84b986
Allan James Vestal <allanjamesvestal@gmail.com>
2019-08-22 12:37:21 -0400

    Modify setup.py: blacken, change to binary version of psycopg2 dependency, use variable for version.

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-from setuptools import find_packages, setup
+# Imports from python.
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+# Imports from government.
+from government import __version__
+
+
+REPO_URL = "https://github.com/The-Politico/politico-civic-government/"
+
+PYPI_VERSION = ".".join(str(v) for v in __version__)
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
 
 setup(
     name="politico-civic-government",
-    version="0.1.5",
-    description="",
-    url="https://github.com/The-Politico/politico-civic-government",
+    version=PYPI_VERSION,
+    packages=find_packages(exclude=["docs", "tests", "example"]),
+    license="MIT",
+    description=(
+        "Manage government and party organizations, the POLITICO way."
+    ),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=REPO_URL,
+    download_url="{repo_url}archive/{version}.tar.gz".format(
+        **{"repo_url": REPO_URL, "version": PYPI_VERSION}
+    ),
     author="POLITICO interactive news",
     author_email="interactives@politico.com",
-    license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Framework :: Django",
@@ -22,16 +46,19 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
     ],
     keywords="",
-    packages=find_packages(exclude=["docs", "tests", "example"]),
     install_requires=[
         "django",
         "djangorestframework",
         "dj-database-url",
-        "psycopg2",
+        "psycopg2-binary",
+        "politico-civic-utils",
         "politico-civic-entity",
         "politico-civic-geography",
         "tqdm",
         "us",
     ],
-    extras_require={"test": ["pytest"]},
+    extras_require={
+        "dev": ["sphinx", "sphinxcontrib-django", "sphinx-rtd-theme"],
+        "test": ["pytest"],
+    },
 )
